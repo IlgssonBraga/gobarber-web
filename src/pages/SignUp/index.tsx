98bc5ef90dd17dc295d082eb5226f22b3b115324
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-07 09:19:12 -0300

    feat: validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,9 +10,23 @@ import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
-    function handleSubmit(data: object): void {
-        console.log(data);
-    }
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
 
     return (
         <Container>
@@ -21,7 +36,7 @@ const SignIn: React.FC = () => {
                 <Form onSubmit={handleSubmit}>
                     <h1>Faça seu cadastro</h1>
 
-                    <Input name="nome" icon={FiUser} placeholder="Nome" />
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
                     <Input name="email" icon={FiMail} placeholder="E-mail" />
                     <Input
                         name="password"
