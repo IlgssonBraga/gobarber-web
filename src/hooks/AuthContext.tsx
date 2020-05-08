fef76d4488ba0be8db71bd476d439a001418960d
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-08 08:50:53 -0300

    feat: logout

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -14,6 +14,7 @@ interface AuthState {
 interface AuthContextData {
     user: object;
     signin(credentials: SignInCredentials): Promise<void>;
+    signout(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -42,8 +43,15 @@ export const AuthProvider: React.FC = ({ children }) => {
         setData({ token, user });
     }, []);
 
+    const signout = useCallback(() => {
+        localStorage.removeItem('@GoBarber:token');
+        localStorage.removeItem('@GoBarber:user');
+
+        setData({} as AuthState);
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ user: data.user, signin }}>
+        <AuthContext.Provider value={{ user: data.user, signin, signout }}>
             {children}
         </AuthContext.Provider>
     );
