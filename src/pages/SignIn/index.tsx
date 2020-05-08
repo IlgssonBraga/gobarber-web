12ad3d6c40dfec88c7f7e8824279ad5b165434b0
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-08 08:45:01 -0300

    feat: useAuth

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -6,7 +6,7 @@ import * as Yup from 'yup';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErros';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
@@ -18,7 +18,7 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
-    const { user, signin } = useContext(AuthContext);
+    const { user, signin } = useAuth();
     console.log(user);
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
