90e67295d698c80e60c6853f215866c89b2524fe
IlgssonBraga <ilgssonbraga@gmail.com>
2020-05-23 12:38:09 -0300

    feat: header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import logo from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/AuthContext';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+    const { user, signout } = useAuth();
+
+    return (
+        <Container>
+            <Header>
+                <HeaderContent>
+                    <img src={logo} alt="GoBarber" />
+                    <Profile>
+                        <img
+                            src={
+                                user.avatar_url ||
+                                'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/240px-User_icon_2.svg.png'
+                            }
+                            alt={user.name}
+                        />
+                        <div>
+                            <span>Bem vindo,</span>
+                            <strong>{user.name}</strong>
+                        </div>
+                    </Profile>
+                    <button type="button" onClick={signout}>
+                        <FiPower />
+                    </button>
+                </HeaderContent>
+            </Header>
+        </Container>
+    );
 };
 
 export default Dashboard;
