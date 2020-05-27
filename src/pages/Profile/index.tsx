4298c962242e9a96b108c9d69806d540458adade
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-27 15:11:47 -0300

    feat: update profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -18,6 +18,8 @@ interface ProfileFormData {
     name: string;
     email: string;
     password: string;
+    old_password: string;
+    password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
@@ -35,18 +37,49 @@ const Profile: React.FC = () => {
                     email: Yup.string()
                         .required('E-mail obrigatório')
                         .email('Digite um e-mail válido'),
-                    password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+                    old_password: Yup.string(),
+                    password: Yup.string().when('old_password', {
+                        is: (val) => !!val.length,
+                        then: Yup.string().required('Campo obrigatório'),
+                        otherwise: Yup.string(),
+                    }),
+                    password_confirmation: Yup.string()
+                        .when('old_password', {
+                            is: (val) => !!val.lenght,
+                            then: Yup.string().required('Campo obrigatório'),
+                            otherwise: Yup.string(),
+                        })
+                        .oneOf(
+                            [Yup.ref('password'), null],
+                            'Confirmação incorreta',
+                        ),
                 });
 
                 await schema.validate(data, {
                     abortEarly: false,
                 });
 
-                await api.post('/users', data);
+                const formData = {
+                    name: data.name,
+                    email: data.email,
+                    ...(data.old_password
+                        ? {
+                              old_password: data.old_password,
+                              password: data.password,
+                              password_confirmation: data.password_confirmation,
+                          }
+                        : {}),
+                };
+
+                const response = await api.put('/profile', formData);
+
+                updateUser(response.data);
+
                 addToast({
                     type: 'success',
-                    title: 'Cadastro realizado com sucesso',
-                    description: 'Você já pode fazer seu logon',
+                    title: 'Perfil atualizado com sucesso',
+                    description:
+                        'Sua informações do perfil foram atualizadas com sucesso!',
                 });
 
                 history.push('/');
@@ -59,9 +92,9 @@ const Profile: React.FC = () => {
 
                 addToast({
                     type: 'error',
-                    title: 'Erro no cadastro',
+                    title: 'Erro no atualização',
                     description:
-                        'Ocorreu um erro ao fazer o cadastro na aplicação, tente novamente.',
+                        'Ocorreu um erro ao fazer a atualização dos seus dados, tente novamente.',
                 });
             }
         },
@@ -79,6 +112,7 @@ const Profile: React.FC = () => {
                     addToast({
                         type: 'success',
                         title: 'Avatar atualizado!',
+                        description: 'Perfil atualizado com sucesso',
                     });
                 });
             }
@@ -119,12 +153,6 @@ const Profile: React.FC = () => {
 
                     <Input name="name" icon={FiUser} placeholder="Nome" />
                     <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input
-                        name="password"
-                        icon={FiLock}
-                        type="password"
-                        placeholder="Senha"
-                    />
 
                     <Input
                         containerStyle={{ marginTop: 24 }}
